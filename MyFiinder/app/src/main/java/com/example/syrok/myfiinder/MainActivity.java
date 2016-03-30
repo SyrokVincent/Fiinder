002e60402b7bbd80ed5ad1df58e8b3861962fb52
Syrok <syrok.vincent@hotmail.fr>
2016-03-30 17:37:55 +0200

    Maj start
    c'est encore le bordel

--- MyFiinder/app/src/main/java/com/example/syrok/myfiinder/MainActivity.java
+++ MyFiinder/app/src/main/java/com/example/syrok/myfiinder/MainActivity.java
@@ -1,52 +1,47 @@
 package com.example.syrok.myfiinder;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    private ListView mListInteret = null;
+    private Button mSend = null;
+    private String[] mInteret = null;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        mListInteret = (ListView) findViewById(R.id.listInteret);
+        mSend = (Button) findViewById(R.id.send);
+
+        mInteret = new String[]{"Cinema", "Manger", "Restaurant", "Sport"};
+        mListInteret.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_multiple_choice, mInteret));
+        mListInteret.setItemChecked(1, true);
+
+        mSend.setOnClickListener(new View.OnClickListener() {
+
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Merci ! Les données ont été envoyées !", Toast.LENGTH_LONG).show();
+
+
+
+                //On déclare qu'on ne peut plus sélectionner d'élément
+                mListInteret.setChoiceMode(ListView.CHOICE_MODE_NONE);
+                //On affiche un layout qui ne permet pas de sélection
+                mListInteret.setAdapter(new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, mInteret));
+
+                //On désactive le bouton
+                mSend.setEnabled(false);
             }
         });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
